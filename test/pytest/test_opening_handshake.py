78656fa8581cb9a5393a37d2a3a68d9bc01dad3f
Jacob Champion <champion.p@gmail.com>
2015-10-27 12:08:38 -0700

    Move agent_10 into testutil.fixtures

--- test/pytest/test_opening_handshake.py
+++ test/pytest/test_opening_handshake.py
@@ -1,11 +1,10 @@
 import pytest
 import re
-import urlparse
 
 from twisted.internet import reactor
 from twisted.web import client
 
-from testutil.fixtures import agent
+from testutil.fixtures import agent, agent_10
 from testutil.websocket import assert_successful_upgrade, make_authority, \
                                make_root, make_request, HOST, HOST_IPV6
 
@@ -49,56 +48,9 @@ def assert_headers_match(actual_headers, expected_list):
 
     assert actual_list == expected_list
 
-class HTTP10Agent:
-    """
-    A hacky attempt at an HTTP/1.0 version of t.w.c.Agent.  Unfortunately
-    t.w.c.Agent only supports HTTP/1.1, so we have to create this ourselves. It
-    uses the old HTTPClientFactory implementation in Twisted.
-
-    Note that this only sort of implements Agent (it doesn't callback until the
-    response is received, and it doesn't even return the full response from
-    request()) and is really only useful for the purposes of these tests.
-    """
-    def __init__(self, reactor):
-        self._reactor = reactor
-
-    class _FakeResponse:
-        def __init__(self, code):
-            self.code = code
-
-    def request(self, method, uri, headers=None, bodyProducer=None):
-        url = urlparse.urlparse(uri, scheme='http')
-        host = url.hostname
-        port = url.port
-
-        if port is None:
-            port = 443 if (url.scheme == 'https') else 80
-
-        # Translate from Agent's Headers object back into a dict.
-        if headers is not None:
-            old_headers = {}
-            for name, value_list in headers.getAllRawHeaders():
-                old_headers[name] = value_list[0]
-            headers = old_headers
-
-        f = client.HTTPClientFactory(uri, method=method, headers=headers,
-                                     timeout=2)
-
-        def gotResponse(page):
-            return HTTP10Agent._FakeResponse(int(f.status))
-        f.deferred.addBoth(gotResponse)
-
-        self._reactor.connectTCP(host, port, f)
-
-        return f.deferred
-
 #
 # Fixtures
 #
-
-@pytest.fixture
-def agent_10():
-    return HTTP10Agent(reactor)
 
 @pytest.yield_fixture(params=['7', '8', '13'])
 def success_response(agent, request):
--- test/pytest/testutil/fixtures.py
+++ test/pytest/testutil/fixtures.py
@@ -1,10 +1,63 @@
 import pytest
+import urlparse
 
 from twisted.internet import reactor
 from twisted.web import client
+
+class _HTTP10Agent:
+    """
+    A hacky attempt at an HTTP/1.0 version of t.w.c.Agent.  Unfortunately
+    t.w.c.Agent only supports HTTP/1.1, so we have to create this ourselves. It
+    uses the old HTTPClientFactory implementation in Twisted.
+
+    Note that this only sort of implements Agent (it doesn't callback until the
+    response is received, and it doesn't even return the full response from
+    request()) and is really only useful for the purposes of these tests.
+    """
+    def __init__(self, reactor):
+        self._reactor = reactor
+
+    class _FakeResponse:
+        def __init__(self, code):
+            self.code = code
+
+    def request(self, method, uri, headers=None, bodyProducer=None):
+        url = urlparse.urlparse(uri, scheme='http')
+        host = url.hostname
+        port = url.port
+
+        if port is None:
+            port = 443 if (url.scheme == 'https') else 80
+
+        # Translate from Agent's Headers object back into a dict.
+        if headers is not None:
+            old_headers = {}
+            for name, value_list in headers.getAllRawHeaders():
+                old_headers[name] = value_list[0]
+            headers = old_headers
+
+        f = client.HTTPClientFactory(uri, method=method, headers=headers,
+                                     timeout=2)
+
+        def gotResponse(page):
+            return _HTTP10Agent._FakeResponse(int(f.status))
+        f.deferred.addBoth(gotResponse)
+
+        self._reactor.connectTCP(host, port, f)
+
+        return f.deferred
+
+
+#
+# Fixtures
+#
 
 @pytest.fixture
 def agent():
     """Returns a t.w.c.Agent for use by tests."""
     return client.Agent(reactor)
 
+@pytest.fixture
+def agent_10():
+    """Returns an HTTP/1.0 "Agent"."""
+    return _HTTP10Agent(reactor)
